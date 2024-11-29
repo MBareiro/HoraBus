35e460a586843e5e5c2ce00fb364f9f2b9d83000
mbareiro <mbareiro.dev@gmail.com>
2024-11-29 14:26:13 -0300

    Cambio en config.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -13,14 +13,14 @@ module.exports = {
   test: {
     username: process.env.DB_USER || "root",
     password: process.env.DB_PASSWORD || null,
-    database: process.env.DB_TEST_NAME || "database_test",
+    database: process.env.DB_NAME || "database_test",
     host: process.env.DB_HOST || "127.0.0.1",
     dialect: "mysql"
   },
   production: {
     username: process.env.DB_USER || "root",
     password: process.env.DB_PASSWORD || null,
-    database: process.env.DB_PROD_NAME || "database_production",
+    database: process.env.DB_NAME || "database_production",
     host: process.env.DB_HOST || "127.0.0.1",
     dialect: "mysql"
   }
