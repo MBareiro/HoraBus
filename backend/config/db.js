4e946e4f8904272b04237b8915b2af6e15ca7778
MBareiro <58647378+MBareiro@users.noreply.github.com>
2024-12-15 03:18:28 -0300

    Modificacion db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,5 @@
 const { Sequelize } = require('sequelize');
 const config = require('./config')[process.env.NODE_ENV || 'development']; // Selecciona configuración según el entorno
-console.log(process.env.NODE_ENV || 'development');
 
 // Crear una nueva instancia de Sequelize con la configuración
 const sequelize = new Sequelize(config.database, config.username, config.password, {
@@ -8,6 +7,7 @@ const sequelize = new Sequelize(config.database, config.username, config.passwor
   dialect: config.dialect,
   logging: config.logging || false, // Habilitar/deshabilitar logs de consultas SQL
   dialectOptions: config.dialectOptions, // Opciones adicionales de dialecto si es necesario
+  dialectModule: require('mysql2'), 
 });
 
 // Función para autenticar la conexión a la base de datos
