275b5082b13984c6b297776383dc9f0e141986bf
ludmila.almiron <ludmi.almi@gmail.com>
2025-01-15 11:58:14 -0300

    login

--- frontend/src/components/userAccess/UserAccess.jsx
+++ frontend/src/components/userAccess/UserAccess.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react"
 import { Login } from "../login/Login"
+import './UserAccess.css'
+import logo from '../../pictures/horabus2.png';
 
 export const UserAccess = () => {
 
@@ -9,10 +11,16 @@ const handleIsOpen = () => {
 setIsOpen(true)
 }
     return(
-        <div>
-            <button onClick={handleIsOpen}>LOGIN</button>
+        <div className="access-conteiner">
+            <div className="logo-container">
+                    <img src={logo} alt="Horabus Logo" className="logo" />
+                  </div>
+            <div className="conteiner-log-reg">
+            <button onClick={handleIsOpen} className="button-log-reg">INICIAR SESIÓN</button>
             <Login isOpen={isOpen} closeModal={setIsOpen}/>
-            <button>REGISTER</button>
+            <button className='inicio-button'>VOLVER A INICIO</button>
+            </div>
+            
         </div>
     )
 }
