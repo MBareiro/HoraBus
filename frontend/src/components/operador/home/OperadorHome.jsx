dee5a3f747c3c709f670db7350288d7e760b30a3
Ludmila Almiron <ludmialmi@gmail.com>
2025-01-16 17:16:33 -0300

    operador

--- frontend/src/components/operador/home/OperadorHome.jsx
+++ frontend/src/components/operador/home/OperadorHome.jsx
@@ -1,12 +1,29 @@
+import { useEffect } from 'react';
 import logo from '../../../pictures/horabus2.png';
 import './OperadorHome.css'
+import { useDispatch } from 'react-redux';
+import { setAccess } from '../../../redux/slices/operadorSlice';
+import { Menu } from '../menu/Menu';
 
 export const OperadorHome = () => {
+const dispatch = useDispatch()
+
+  useEffect(()=>{
+    const {access} = 
+        JSON.parse(localStorage.getItem("auth"))
+    dispatch(setAccess(access))
+  }, [])
     return (
         <div className="app">
+             <div className="access-conteiner">
+                           <div>
+                            <button className="button-operador">ROL OPERADOR</button>
+                            </div>
+                    </div>
             <div className="logo-container-operador">
                     <img src={logo} alt="Horabus Logo" className="logo" />
                   </div>
+                  <Menu/>
         </div>
     )
 }
---
+++ frontend/src/components/operador/menu/Menu.jsx
@@ -0,0 +1,12 @@
+
+
+export const Menu = () => {
+    return(
+        <div className="select-container">
+        <div className="select-container-buttons">
+            <button className="selector">PARADAS</button>
+            <button className="selector">HORARIOS</button>
+        </div>
+  </div>
+    )
+}
