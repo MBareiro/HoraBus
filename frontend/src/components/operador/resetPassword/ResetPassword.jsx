e49b12e18b7b27bb9aba96c39a282b61d3efd99b
Ludmila Almiron <ludmialmi@gmail.com>
2025-01-23 14:36:03 -0300

    reset password design

--- frontend/src/components/operador/resetPassword/ResetPassword.jsx
+++ frontend/src/components/operador/resetPassword/ResetPassword.jsx
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
 import { getNewPassword } from "../../../redux/actions/operadorActions/operadorActions";
 import { setRecoverPassword, setSignIn } from "../../../redux/slices/operadorSlice";
+import "./ResetPassword.css"
+import loadingGif from '../../../pictures/loading.gif'
 
 export const ResetPassword = () => {
     const { token } = useParams();
@@ -11,6 +13,7 @@ export const ResetPassword = () => {
     const navigate = useNavigate()
 
     const [message, setMessage] = useState("")
+    const [showLoadinGif, setShowLoadinGif] = useState(false)
 
     const [newPassword, setNewPassword]= useState({
         newPassword: "",
@@ -25,10 +28,12 @@ export const ResetPassword = () => {
     }
 
     const handleNewPassword = () => {
+    setShowLoadinGif(true)
     dispatch(getNewPassword(newPassword))
     }
 
     useEffect(() => {
+        setShowLoadinGif(false)
         setMessage(recoverPassword.message)
     }, [recoverPassword])
 
@@ -39,22 +44,27 @@ export const ResetPassword = () => {
     }
 
     return(
-        <div>
-            <div>
-            <form>
-                <label>INGRESE SU NUEVA CONTRASEÑA</label>
+        <div className="conteiner-reset-password">
+            <div className="conteiner-form-reset">
+            <form className="conteiner-form">
+                <label className="label-reset">INGRESE SU NUEVA CONTRASEÑA</label>
                 <input
                 type="password"
                 name="newPassword"
                 id="newPassword"
                 onChange={handleChange}
-                value={newPassword.newPassword}></input>
+                value={newPassword.newPassword}
+                className="input-recover"></input>
             </form>
-            </div>
-            <button onClick={handleNewPassword}>ENVIAR</button>
-            {message && <div>
-                <span>{message}</span> <button onClick={handleSignIn}> INICIAR SESION </button> 
+            <button onClick={handleNewPassword}  className="button-recover">ENVIAR</button>
+            {showLoadinGif && <img src={loadingGif} alt="Cargando..." className="loading-gif-recover" />}  
+            {message && 
+            <div className="conteiner-form-reset">
+                <span className="span-recover">{message}</span> 
+                <button onClick={handleSignIn} className="button-recover"> INICIAR SESION </button> 
                 </div>}
+            </div>
+            
         </div>
     )
 }
