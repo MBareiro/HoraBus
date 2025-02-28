686f89157b17685bfd362879c23e2ff9006bc4db
MBareiro <mbareiro.dev@gmail.com>
2025-02-28 15:51:10 -0300

    Remove custom CSS URL from Swagger UI setup

--- backend/config/swaggerConfig.js
+++ backend/config/swaggerConfig.js
@@ -12,12 +12,11 @@ const options = {
   },
   apis: ['./src/routes/*.js'], 
 };
-// CDN CSS
-const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+
 const swaggerSpec = swaggerJSDoc(options);
 
 const setupSwagger = (app) => {
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec,  { customCssUrl: CSS_URL }));
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 };
 
 module.exports = setupSwagger;
